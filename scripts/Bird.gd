6eafe3374b16f4ce08f9fbf3bebd5c9dc7dbb533
Fabrice Jeannet <fabricejeannet@fabrice.coolworking.fr>
2022-05-19 10:11:11 +0200

    Backup avant refonte

--- scripts/Bird.gd
+++ scripts/Bird.gd
@@ -8,9 +8,11 @@ var birds_in_sight = []
 
 var margin:int
 
-export var speed:int = 10
-export var turn_factor = 1
-export var centering_factor:float = 0.005
+export var speed = 1.2
+export var turn_factor = 1.0
+export var centering_factor:float = 0.05
+export var avoid_factor:float = 0.5
+export var min_distance = 50
 
 func _ready():
 	margin = get_parent().margin
@@ -26,13 +28,15 @@ func set_number(n:int) -> void :
 
 
 func _physics_process(delta):
+	compute_motion()
+	avoid_other_boids()
+
 	keep_within_bounds()
-	move_and_slide(motion.normalized() * speed)
-
-
-func _draw():	
-	draw_line(position, motion.normalized() , Color.white)
-#
+	move_local_x(motion.normalized().x * speed)
+	move_local_y(motion.normalized().y * speed)
+#func _draw():	
+#	draw_line(position, motion.normalized() , Color.white)
+##
 
 func keep_within_bounds() -> void:
 	if position.x < margin :
@@ -48,21 +52,12 @@ func keep_within_bounds() -> void:
 func _on_Sigth_body_entered(body):
 	if body != self :
 		birds_in_sight.append(body)
-		update_label()
-		compute_motion()
+
 
 func _on_Sigth_body_exited(body):
 	if body != self :
 		birds_in_sight.remove(birds_in_sight.find(body))
-		update_label()
-		compute_motion()
 	
-func update_label():
-	var t = "["
-#	for bird in birds_in_sight:
-#		t += str(bird.number) + ", "
-#	t += "]"
-#	$Label.text = str(number) + t
 	
 func compute_motion():
 	var x_sum = 0.0
@@ -70,10 +65,19 @@ func compute_motion():
 	for bird in birds_in_sight:
 		x_sum += bird.position.x
 		y_sum += bird.position.y
+	
+	if birds_in_sight.size() > 0 :	
+		var center_of_mass = Vector2 (x_sum / float(birds_in_sight.size()), y_sum / float(birds_in_sight.size()))
+		motion = Vector2((center_of_mass.x - position.x) * centering_factor, (center_of_mass.y - position.y) * centering_factor)
+		rotate(get_angle_to(center_of_mass))
+
+func avoid_other_boids():
+	var move_x = 0.0
+	var move_y = 0.0
+	for bird in birds_in_sight:
+		if bird.position.distance_to(self.position) < min_distance:
+			move_x += self.position.x - bird.position.x
+			move_y += self.position.y - bird.position.y
+		motion.x += move_x * avoid_factor
+		motion.y += move_y * avoid_factor
 		
-	var center_of_mass = Vector2 (int(x_sum / float(get_child_count())), int(y_sum / float(get_child_count())))
-	
-	motion =  Vector2((center_of_mass.x - position.x) * centering_factor, (center_of_mass.y - position.y) * centering_factor)
-	
-#	for bird in birds_in_sight:
-#		bird.motion = motion
